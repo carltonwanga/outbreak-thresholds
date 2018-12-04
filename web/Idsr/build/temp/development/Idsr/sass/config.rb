3cc17ff23a81e20a9507e0d5829d5e83ca51b811
Carlton <carltonwanga@gmail.com>
2018-12-04 17:32:48 +0300

    Ext Js set up

---
+++ web/Idsr/build/temp/development/Idsr/sass/config.rb
@@ -0,0 +1,2 @@
+Compass.add_project_configuration('..\..\..\..\..\sass\config.rb')
+cache_path = 'D:\work\instedd\dsru-web\idsr\web\Idsr/build/.sass-cache'
---
+++ web/Idsr/build/temp/production/Idsr/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,3 @@
+// @tag full-page
+// @require D:\work\instedd\dsru-web\idsr\web\Idsr\raphael.min.js
+// @require D:\work\instedd\dsru-web\idsr\web\Idsr\app.js
