d5c7dd153016a496b856d617c3a05351848fe72c
Carlton <carltonwanga@gmail.com>
2018-12-04 18:08:46 +0300

    Add front end utility functions

---
+++ web/Idsr/app/util/Constants.js
@@ -0,0 +1,26 @@
+/**
+ * Created by PAVILION 15 on 10/8/2018.
+ */
+Ext.define('Idsr.util.Constants', {
+    statics: {
+        rootUrl: "/idsr/admin/",
+        controllersApiFromIndex: "../app",
+        apiUrl: "",
+        signInUrl:"../app/signin",
+        logoutUrl: "../app/auth/logout",
+        historicDataStartYear:2012,
+        thresholdDataStartYear:2018,
+        historicDataEndYear:2015
+    }
+
+    /*statics: {
+        rootUrl: "/idsr/Idsr/",
+        controllersApiFromIndex: "../app",
+        apiUrl: "",
+        signInUrl:"../app/signin",
+        logoutUrl: "../app/auth/logout",
+        historicDataStartYear:2012,
+        thresholdDataStartYear:2018,
+        historicDataEndYear:2015
+    }*/
+});
